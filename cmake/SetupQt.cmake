1a51001a0ab28126db87c4d98af89382127d8884
Michael Fisher <mfisher@kushview.net>
2026-03-29 20:45:14 -0400

    feat: reuse compliance

--- cmake/SetupQt.cmake
+++ cmake/SetupQt.cmake
@@ -1,3 +1,5 @@
+# Copyright (C) 2026 Medical Informatics Engineering.
+# SPDX-License-Identifier: LGPL-3.0-or-later
 
 # Auto-discover Qt from the standard Qt Community installer location (~Qt/<version>/macos)
 # so developers don't need to set CMAKE_PREFIX_PATH or any environment variables.
